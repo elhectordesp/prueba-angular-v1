e21e67c146db8c9306bf82fd8ddab422a5c56a14
elhectordesp <hectordura74@gmail.com>
2023-02-04 11:05:53 +0100

    - Implementado "inicio de sesión"

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  logged: boolean = false;
+
+  ngOnInit(): void {
+    const login = sessionStorage.getItem('login');
+    this.logged = login ? JSON.parse(login) : false;
+  }
+
+
+  login() {
+    sessionStorage.setItem('login', this.logged.toString());
+    this.logged = !this.logged;
+  }
 
 }
